01edec98458660a7e44e9e64c9586d83f4983fb9
FH3095 <FH3095@users.noreply.github.com>
2017-07-07 16:35:48 +0200

    Initial files setup

---
+++ locales/deDE.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("RollBot", "deDE")
+if not L then
+	return
+end
+
---
+++ locales/enUS.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("RollBot", "enUS", true, false)
+if not L then
+	return
+end
+
